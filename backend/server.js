e6e4a630d92bae8cba852d6d0af6b36e5ea8bff5
Sarah Nance <snance44@gmail.com>
2022-08-02 11:45:16 -0500

    fix deploy issue

--- backend/server.js
+++ backend/server.js
@@ -30,13 +30,15 @@ app.use("/api/desserts", dessertRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running....");
   });
 }
 
@@ -48,6 +50,6 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-const PORT=process.env.port||5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode port ${PORT}`));
+
+app.listen(process.env.PORT || 5000);
